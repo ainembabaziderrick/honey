409ab9281a8389061ecb369e2c021e4ec705d123
ainembabaziderrick <ceoainembabaziderrick@gmail.com>
2024-04-07 16:22:34 +0300

    login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',[AuthController::class,'Login']);
+Route::get('forgot',[AuthController::class,'forgot']);
+Route::post('login_post',[AuthController::class,'login_post']);
+
+Route::post('forgot_post',[AuthController::class,'forgot_post']);
+
+Route::group(['middleware' => 'admin'],function(){
+
+//admin
+
+Route::get('admin/dashboard',[DashboardController::class,'Dashboard']);
+
+
 });
+
+Route::get('admin/dashboard',[DashboardController::class,'Dashboard']);
+
+
+Route::get('logout',[AuthController::class,'logout']);
